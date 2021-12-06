1ed8d919bb00048f6637bbb584e4cf39160db2b9
maqikun <912569147@qq.com>
2021-12-06 15:27:14 +0800

    FEAT: add analysis data to mysql --contributed-by: qikunma

--- TCService-analysis/src/main/java/com/mqk/analysis/io/MysqlTextOutputFormat.java
+++ TCService-analysis/src/main/java/com/mqk/analysis/io/MysqlTextOutputFormat.java
@@ -10,7 +10,10 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -20,9 +23,64 @@ public class MysqlTextOutputFormat extends OutputFormat<Text, Text> {
 
 	protected static class MySQLRecordWriter<K,V> extends RecordWriter<Text,Text>{
 		private Connection connection;
-		public MySQLRecordWriter() {
+		private Map<String, Integer> userMap = new HashMap<>();
+		private Map<String, Integer> dateMap = new HashMap<>();
+
+		public MySQLRecordWriter(){
 			//获取资源
 			connection = JDBCUtil.getConnection();
+			//读取用户，时间数据
+			PreparedStatement preparedStatement = null;
+			ResultSet rs = null;
+			try {
+				//查询用户
+				String sqlForQueryUser = "select id, tel from ct_user";
+				preparedStatement = connection.prepareStatement(sqlForQueryUser);
+				rs = preparedStatement.executeQuery();
+				while (rs.next()){
+					int id = rs.getInt(1);
+					String tel = rs.getString(2);
+					userMap.put(tel, id);
+				}
+				rs.close();
+
+				//查询时间
+				String sqlForQueryDate = "select id, year, month, day from ct_date";
+				preparedStatement = connection.prepareStatement(sqlForQueryDate);
+				rs = preparedStatement.executeQuery();
+				while (rs.next()){
+					final int id = rs.getInt(1);
+					final String year = rs.getString(2);
+					String month = rs.getString(3);
+					if(month.length() == 1){
+						month = "0" + month;
+					}
+					String day = rs.getString(4);
+					if(day.length() == 1){
+						day = "0" + day;
+					}
+					dateMap.put(year + month + day, id);
+				}
+				rs.close();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				if(null != rs){
+					try {
+						rs.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(null != preparedStatement){
+					try {
+						preparedStatement.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		}
 
 		/**
@@ -39,8 +97,14 @@ public class MysqlTextOutputFormat extends OutputFormat<Text, Text> {
 			try {
 				String sql = "insert into ct_call (telid, dateid, sumcall, sumduration) values (?, ?, ?, ?)";
 				preparedStatement = connection.prepareStatement(sql);
-				preparedStatement.setInt(1, 2);
-				preparedStatement.setInt(2, 3);
+
+				String k = key.toString();
+				final String[] ks = k.split("_");
+				String tel = ks[0];
+				String date = ks[1];
+
+				preparedStatement.setInt(1, userMap.get(tel));
+				preparedStatement.setInt(2, dateMap.get(date));
 				preparedStatement.setInt(3, Integer.parseInt(values[0]));
 				preparedStatement.setInt(4, Integer.parseInt(values[1]));
 				preparedStatement.executeUpdate();
